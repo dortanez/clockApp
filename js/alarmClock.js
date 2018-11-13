1a57f8f4c2a99d378f070a0ab338a13744762ecb
Danisa Ortanez <dortanez@yahoo.com>
2018-11-12 19:30:26 -0800

    adding

--- js/alarmClock.js
+++ js/alarmClock.js
@@ -1,7 +1,18 @@
-function selectTime() {
+var hrSelect = document.getElementById("alarmHrs");
+var minSelect = document.getElementById("alarmMins");
+var pdSelect = document.getElementById("alarmPeriod");
 
-	//SELECTING HOURS/////////////////////////////////////
-	var hrSelect = document.getElementById("alarmHrs");
+ function addZero(i) {
+ 	if(i < 10) {
+ 		return "0" + i;
+ 	} else {
+ 		return i;
+ 	}
+ }
+
+function addOptions() {
+
+	//ADDING HOURS/////////////////////////////////////
 	var hrs = 12;
 	for(i = 1; i <= hrs; i++) {
 		var hrOption = document.createElement("option");
@@ -9,8 +20,7 @@ function selectTime() {
 		hrSelect.appendChild(hrOption);
 	}
 
-	//SELECTING MINUTES//////////////////////////////////
-	var minSelect = document.getElementById("alarmMins");
+	//ADDING MINUTES//////////////////////////////////
 	var mins = 59;
 	for(i = 0; i <= mins; i++) {
 		var minOption = document.createElement("option");
@@ -18,25 +28,48 @@ function selectTime() {
 		minSelect.appendChild(minOption);
 	}
 }
-selectTime();
-
-function setAlarm() {
+addOptions();
 
 
+function setTime() {
 
+	var selectedHr = hrSelect.options[hrSelect.selectedIndex].value;
+	var selectedMin = minSelect.options[minSelect.selectedIndex].value;
+	var selectedPd = pdSelect.options[pdSelect.selectedIndex].value;
 
+	//SELECTED ALARM SET////////////////////////////////
+	var alarmTime = selectedHr + ":" + selectedMin + " " + selectedPd;
+	console.log(alarmTime);
 
+	var alarmSet = document.getElementById("alarmSet");
+	alarmSet.innerText = "alarm is set for " + alarmTime;
 
+	//GENERATE CURRENT TIME/////////////////////////////
+	setInterval(function() {
+	var now = new Date();
+	var hours = (now.getHours() > 12) ? now.getHours() - 12 : now.getHours();
+	var minutes = now.getMinutes();
+	var period = (now.getHours() < 12) ? "AM" : "PM";
 
+	var currentTime = addZero(hours) + ":" + addZero(minutes) + " " + period ;
+	console.log(currentTime);
 
+	//COMPARE ALARM AND CURRENT TIME////////////////////
+	if(alarmTime == currentTime) {
+		console.log("BEEEEEP");
+		alert("BEEEEEP BOOOOP BEEEEEP");
+		
+	}
 
+	},1000)
 
 	
 
-	var now = new Date();
-	var hours = now.getHours();
-	var minutes = now.getMinutes();
-	var period = "AM";
-
 
 }
+
+
+ 
+
+
+
